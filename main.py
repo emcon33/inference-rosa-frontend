7432adf1885c8733ba2489d46bc33fe5629121e1
Andy G <48636570+emcon33@users.noreply.github.com>
2024-03-07 09:15:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 # Set URL
 # Run: 
 # update REST_API_URL to your backend link (currently not dynamic)
-REST_API_URL = "https://inference-rosa-workshop-test.apps.rosa-cdvbq.iuuc.p1.openshiftapps.com/api/predict"
+REST_API_URL = "https://inference-rosa-workshop-test.apps.rosa-qjtmr.9y8u.p1.openshiftapps.com/api/predict"
 
 # Inference!
 def inference(image_path):
